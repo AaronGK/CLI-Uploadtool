a24c42671bb2588673d8b906c17f749c3a5b49e1
Aaron Kennedy <ak@MacBookAir.lan>
2024-12-31 15:30:01 +0000

    Alteration of terraform configuration to write to .env in root folder

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
   required_version = ">= 1.3.0"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
-output "env_file" {
-  value = <<EOT
+resource "local_file" "env_file" {
+  content  = <<EOT
 AZURE_CLIENT_ID=${azuread_application.app.application_id}
 AZURE_CLIENT_SECRET=${azuread_application_password.password.value}
 AZURE_TENANT_ID=${data.azurerm_client_config.current.tenant_id}
@@ -7,5 +7,5 @@ AZURE_SUBSCRIPTION_ID=${data.azurerm_client_config.current.subscription_id}
 AZURE_STORAGE_ACCOUNT=${azurerm_storage_account.storage.name}
 AZURE_STORAGE_CONTAINER=${azurerm_storage_container.container.name}
 EOT
-  sensitive = true
+  filename = "${path.module}/../.env"
 }
